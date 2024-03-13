73fd6340109be703a47651a390a90c2326841421
VitorHLima <vitor.oliveira@sptech.school>
2024-03-13 20:20:20 -0300

    Subindo versão nova

--- banco/banco_v1.sql
+++ banco/banco_v1.sql
@@ -1,8 +1,14 @@
+drop database projeto_pi;
 CREATE DATABASE projeto_pi;
+use projeto_pi;
+
 
 CREATE TABLE usuario (
 	idUsuario INT auto_increment primary key,
-    cpnj CHAR(18),
     email varchar(50),
-    senha varchar(50)
+    senha varchar(50),
+    cnpj char(17),
+    nome varchar(100)
 );
+
+select * from usuario;
