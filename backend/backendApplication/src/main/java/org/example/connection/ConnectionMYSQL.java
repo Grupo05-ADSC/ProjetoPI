8e31b144cd8e8300ca30007a7de459968ae14bff
carlosOsilva <carlos.osilva@sptech.school>
2024-06-10 16:27:44 -0300

    Resolvendo commit errado

--- backend/backendApplication/src/main/java/org/example/connection/ConnectionMYSQL.java
+++ backend/backendApplication/src/main/java/org/example/connection/ConnectionMYSQL.java
@@ -6,7 +6,11 @@ public class ConnectionMYSQL extends Conexao {
 
     private static final String URL_MYSQL = "jdbc:mysql://localhost/sisguard";
     private static final String USERNAME_MYSQL = "root";
+<<<<<<< HEAD
     private static final String PASSWORD_MYSQL = "28428510";
+=======
+    private static final String PASSWORD_MYSQL = "M!ch3l1y";
+>>>>>>> parent of 695cc1e (Individual v3)
 
     @Override
     protected Connection getConexaoEspecifica() throws SQLException {
